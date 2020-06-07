734e08ae3dd5a9c0178d3666da3d2a232794630d
Gaurav Bagul <gauravsbagul@gmail.com>
2020-06-07 18:20:45 +0530

    handled error

--- currency-convert.js
+++ currency-convert.js
@@ -5,20 +5,31 @@
 
 const axios = require("axios");
 
-const getExchangeRate = (from, to) => {
-    return axios
-        .get(`http://api.fixer.io/latest?base=${from}`)
-        .then((response) => {
-            return response.data.rates[to];
-        });
+// Unable to get exchange rate for USD and CAD.
+const getExchangeRate = async(from, to) => {
+    try {
+        const response = await axios.get(`http://api.fixer.io/latest?base=${from}`);
+        const rate = response.data.rates[to];
+
+        if (rate) {
+            return rate;
+        } else {
+            throw new Error();
+        }
+    } catch (e) {
+        throw new Error(`Unable to get exchange rate for ${from} and ${to}.`);
+    }
 };
 
-const getCountries = (currencyCode) => {
-    return axios
-        .get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
-        .then((response) => {
-            return response.data.map((country) => country.name);
-        });
+const getCountries = async(currencyCode) => {
+    try {
+        const response = await axios.get(
+            `https://restcountries.eu/rest/v2/currency/${currencyCode}`
+        );
+        return response.data.map((country) => country.name);
+    } catch (e) {
+        throw new Error(`Unable to get countries that use ${currencyCode}.`);
+    }
 };
 
 const convertCurrency = (from, to, amount) => {
@@ -47,6 +58,10 @@ const convertCurrencyAlt = async(from, to, amount) => {
   )}`;
 };
 
-convertCurrencyAlt("USD", "CAD", 100).then((status) => {
-    console.log(status);
-});
+convertCurrencyAlt("USD", "EUR", 100)
+    .then((status) => {
+        console.log(status);
+    })
+    .catch((e) => {
+        console.log(e.message);
+    });
